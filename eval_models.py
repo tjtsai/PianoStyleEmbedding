20f2f224f477e89924d12edab4f6774ecfc2cd6b
TJ Tsai <tjtsai@g.hmc.edu>
2020-02-01 22:14:32 -0800

    Adding 03_awdlstm.ipynb for training AWD-LSTM model without any pre-training.  Also added eval_models.py to provide shared helper functions for evaluating models.

---
+++ eval_models.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from fastai import *
+from fastai.text import *
+
+def calcPagePrior(train_df, valid_df):
+    
+    # get label info
+    df = pd.concat([train_df, valid_df], sort=False)
+    labels = df['label'].tolist()
+    composers = sorted(list(set(labels)))
+    composer2idx = {c:i for i, c in enumerate(composers)}
+    
+    # accumulate & normalize
+    counts = np.zeros(len(composers))
+    for l in labels:
+        counts[composer2idx[l]] += 1
+    priors = counts / np.sum(counts)
+
+    # format
+    priors = torch.from_numpy(priors.reshape((1,-1)))
+    
+    return priors
+
+def calcAccuracy_fullpage(learner, path, train_df, valid_df, test_df, ensembled = False):
+    
+    # batch inference
+    learner.export()
+    learner = load_learner(path, test=TextList.from_df(test_df, path, cols='text'))
+    probs, y = learner.get_preds(ds_type=DatasetType.Test, ordered=True)
+
+    # ground truth labels
+    labels = list(test_df['label'])
+    composers = sorted(set(labels))
+    composer2idx = {c:i for i, c in enumerate(composers)}
+    gt = torch.from_numpy(np.array([composer2idx[l] for l in labels]))
+    
+    # average if ensembled
+    if ensembled:
+        boundaries = getPageBoundaries(test_df)
+        probs, gt = averageEnsembled(probs, gt, boundaries)
+    
+    # apply priors
+    priors = calcPagePrior(train_df, valid_df)
+    probs_with_priors = torch.mul(probs, priors)
+    
+    # calc accuracy
+    acc = accuracy(probs, gt).item()
+    acc_with_prior = accuracy(probs_with_priors, gt).item()
+    
+    return acc, acc_with_prior
+
+def getPageBoundaries(df):
+    queryids = list(df['id'])
+    boundaries = []
+    for i, qid in enumerate(queryids):
+        if qid[-2:] == '_0':
+            boundaries.append(i)
+    return boundaries
+
+def averageEnsembled(probs, gt, boundaries):
+    gt_selected = gt[boundaries]
+    accum = torch.zeros((len(boundaries), probs.shape[1]))
+    for i, bnd in enumerate(boundaries):
+        if i == len(boundaries) - 1:
+            accum[i,:] = torch.sum(probs[bnd:,:], axis=0)
+        else:
+            next_bnd = boundaries[i+1]
+            accum[i,:] = torch.sum(probs[bnd:next_bnd,:], axis=0)
+    return accum, gt_selected
